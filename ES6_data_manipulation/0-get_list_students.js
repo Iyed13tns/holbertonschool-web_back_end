f39e4bacab7e4a3f6021cc99e26fc542bed4a606
Iyed13tns <m@g.com>
2025-03-31 09:25:02 +0200

    refactor: simplify getListStudents function by removing test code

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,7 @@
-import getListStudents from './0-get_list_students';
-
-test('getListStudents returns the correct array of students', () => {
-    const students = getListStudents();
-    expect(students).toEqual([
+export default function getListStudents() {
+    return [
         { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
         { id: 2, firstName: 'James', location: 'Columbia' },
         { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ]);
-});
+    ];
+}
