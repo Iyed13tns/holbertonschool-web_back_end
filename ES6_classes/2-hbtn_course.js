1ba52f58c3ac06d1de397f3d8b147c42a8f2fde5
Iyed13tns <m@g.com>
2025-04-12 13:17:37 -0400

    fix: remove unused getters and setters in HolbertonCourse class

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -19,24 +19,4 @@ export default class HolbertonCourse {
     if (typeof value !== 'string') throw new TypeError('Name must be a string');
     this._name = value;
   }
-
-  get length() {
-    return this._length;
-  }
-
-  set length(value) {
-    if (typeof value !== 'number') throw new TypeError('Length must be a number');
-    this._length = value;
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  set students(value) {
-    if (!Array.isArray(value) || !value.every((s) => typeof s === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = value;
-  }
 }
