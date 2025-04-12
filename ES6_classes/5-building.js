1dada453cb979ab8b4d423e2baf7bffef2d70cb9
Iyed13tns <m@g.com>
2025-04-12 14:08:27 -0400

    fix: remove trailing newline at the end of 9-hoisting.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,10 +3,6 @@ import Building from './5-building';
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
   }
 
   get sqft() {
--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -51,4 +51,3 @@ export class HolbertonClass {
   ];
   
   export default listOfStudents;
-  
