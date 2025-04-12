588e98f81db320c2b9fdc903762e2c132cf77d94
Iyed13tns <m@g.com>
2025-04-12 01:38:48 -0400

    feat: add Car class with constructor and cloning functionality

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+export default class Car {
+    constructor(brand, motor, color) {
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
+    }
+  
+    cloneCar() {
+      const Cloned = this.constructor[Symbol.species] || this.constructor;
+      return new Cloned();
+    }
+  
+    static get [Symbol.species]() {
+      return this;
+    }
+  }
+  
