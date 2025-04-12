8a0dd626142a56ab44b33c0bd9b86ef8500c9f95
Iyed13tns <m@g.com>
2025-04-12 14:14:18 -0400

    refactor: improve formatting and structure in 10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,17 +1,16 @@
 export default class Car {
-    constructor(brand, motor, color) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-    }
-  
-    cloneCar() {
-      const Cloned = this.constructor[Symbol.species] || this.constructor;
-      return new Cloned();
-    }
-  
-    static get [Symbol.species]() {
-      return this;
-    }
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
-  
+
+  cloneCar() {
+    const Cloned = this.constructor[Symbol.species] || this.constructor;
+    return new Cloned();
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+}
