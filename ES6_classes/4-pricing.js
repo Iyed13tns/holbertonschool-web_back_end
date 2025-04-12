dfa11b05a553edc587ef034106f51ace3f683ad0
Iyed13tns <m@g.com>
2025-04-12 13:41:36 -0400

    fix: add type validation for amount and currency in Pricing class

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,7 +1,13 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
     this._amount = amount;
     this._currency = currency;
   }
@@ -11,6 +17,9 @@ export default class Pricing {
   }
 
   set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
     this._amount = value;
   }
 
@@ -19,6 +28,9 @@ export default class Pricing {
   }
 
   set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
     this._currency = value;
   }
 
@@ -27,6 +39,9 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversionRate must be numbers');
+    }
     return amount * conversionRate;
   }
 }
