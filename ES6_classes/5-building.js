b367b868d3b4654e860e6785c440339fdc4cc92a
Iyed13tns <m@g.com>
2025-04-12 13:47:25 -0400

    refactor: remove SkyHighBuilding class implementation from Building file

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -13,18 +13,3 @@ export default class Building {
     return this._sqft;
   }
 }
-
-export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
-    this._floors = floors;
-  }
-
-  get floors() {
-    return this._floors;
-  }
-
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
-  }
-}
